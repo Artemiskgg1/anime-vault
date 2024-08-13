289a354b8aa25df075a280225915bde81499db48
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-08-14 00:28:01 +0530

    chore: Update .gitignore and add new pages for sign-in and sign-up

--- app/(dashboard)/anime-list/page.tsx
+++ app/(dashboard)/anime-list/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div>Please work god thank you</div>;
 };
 
 export default page;
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/anime-list(.*)"]);
+export default clerkMiddleware((auth, req, event) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
