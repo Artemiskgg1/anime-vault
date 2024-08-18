befa2107c3726b50f92e2c985160d6d90c97b237
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-08-18 21:38:22 +0530

    changes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="max-w-7xl h-screen mx-auto  bg-[#0a0a0b]">{children}</div>
-  );
+  return <div className="max-w-[90%] h-screen mx-auto  ">{children}</div>;
 };
 
 export default layout;
